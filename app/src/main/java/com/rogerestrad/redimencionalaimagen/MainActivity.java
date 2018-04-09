45e0872e65a0967bdcf721a07b76e0627b8f35f3
Edgar Rogelio <rogerestrad@gmail.com>
2018-04-09 13:56:14 -0500

    toast

--- app/src/main/java/com/rogerestrad/redimencionalaimagen/MainActivity.java
+++ app/src/main/java/com/rogerestrad/redimencionalaimagen/MainActivity.java
@@ -3,8 +3,11 @@ package com.rogerestrad.redimencionalaimagen;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 ImageView imagen;
@@ -13,10 +16,12 @@ private int iwidth=65;
 private int iheigth=52;
 float density;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         DisplayMetrics displayMetrics= getResources().getDisplayMetrics();
         density=displayMetrics.densityDpi;
         imagen=(ImageView)findViewById(R.id.imageView);
@@ -25,11 +30,16 @@ float density;
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.e("valor","Holllalalalla"+progress);
                 float ancho=(progress+iwidth)*density/160;
                 float alto=(progress+iheigth)*density/160;
                 imagen.getLayoutParams().height=(int)alto;
                 imagen.getLayoutParams().width=(int)ancho;
                 imagen.requestLayout();
+                if ((progress==0)||(progress==100)){
+                    Toast.makeText(MainActivity.this,"se a llegado al limite",Toast.LENGTH_SHORT).show();
+                }
+
 
             }
 
